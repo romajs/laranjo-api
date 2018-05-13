6d2139b2a2d2e4ef8b451efacb4490879e50dcf5
romajs <joseromualdoneto@gmail.com>
2018-05-13 15:21:19 -0300

    Updated npm dependencies

--- test/routeSpec.js
+++ test/routeSpec.js
@@ -7,36 +7,36 @@ var model = require('../src/model')
 describe('Routing message event requests', function () {
   it('Should fail to handle unknown event request', function () {
     return request(app)
-        .get('/')
-        .expect(200)
-        .expect('VO TI DA U SHUTI')
+      .get('/')
+      .expect(200)
+      .expect('VO TI DA U SHUTI')
   })
   it('Should fail to handle unknown event request', function () {
     return request(app)
-        .post('/')
-        .send({
-          'type': 'UNKNOWN'
-        })
-        .expect(500)
-        .expect('Content-Type', /application\/json/)
+      .post('/')
+      .send({
+        'type': 'UNKNOWN'
+      })
+      .expect(500)
+      .expect('Content-Type', /application\/json/)
   })
   it('Should fail to handle event request without type defined', function () {
     return request(app)
-        .post('/')
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
+      .post('/')
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
   })
   it('Should handle added to space event request', function () {
     return request(app)
-        .post('/')
-        .send({
-          'type': 'ADDED_TO_SPACE'
-        })
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-        .expect({
-          'text': 'VO TI DA U SHUTI'
-        })
+      .post('/')
+      .send({
+        'type': 'ADDED_TO_SPACE'
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect({
+        'text': 'VO TI DA U SHUTI'
+      })
   })
   describe('Should handle message event request properly', function () {
     var findOneStub
@@ -51,38 +51,38 @@ describe('Routing message event requests', function () {
         url: 'http://localhost:8000/image/upload/v0123456789/dm8tdGktZGEtdS1zaHV0aQo='
       })
       return request(app)
-          .post('/')
-          .send({
-            'type': 'MESSAGE',
-            'message': {
-              'argumentText': 'VO TI DA U SHUTI'
-            }
-          })
-          .expect(200)
-          .expect('Content-Type', /application\/json/)
-          .expect({
-            'cards': [{
-              'sections': [{
-                'widgets': [{
-                  'image': {
-                    'imageUrl': 'http://localhost:8000/image/upload/v0123456789/dm8tdGktZGEtdS1zaHV0aQo='
-                  }
-                }]
+        .post('/')
+        .send({
+          'type': 'MESSAGE',
+          'message': {
+            'argumentText': 'VO TI DA U SHUTI'
+          }
+        })
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        .expect({
+          'cards': [{
+            'sections': [{
+              'widgets': [{
+                'image': {
+                  'imageUrl': 'http://localhost:8000/image/upload/v0123456789/dm8tdGktZGEtdS1zaHV0aQo='
+                }
               }]
             }]
-          })
+          }]
+        })
     })
     it('With no attachment found', function () {
       findOneStub.resolves(null)
       return request(app)
-          .post('/')
-          .send({
-            'type': 'MESSAGE',
-            'message': {
-              'argumentText': 'VO TI DA U SHUTI'
-            }
-          })
-          .expect(404)
+        .post('/')
+        .send({
+          'type': 'MESSAGE',
+          'message': {
+            'argumentText': 'VO TI DA U SHUTI'
+          }
+        })
+        .expect(404)
     })
   })
 })
